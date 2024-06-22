ef1eb101f1c397fdeeaf8b4d1a234f5d2c1591a7
niklhut <49069026+niklhut@users.noreply.github.com>
2024-06-22 13:50:06 +0200

    Initial student repo

--- src/main/java/h07/Main.java
+++ src/main/java/h07/Main.java
@@ -1,5 +1,10 @@
 package h07;
 
+import org.tudalgo.algoutils.student.annotation.DoNotTouch;
+import org.tudalgo.algoutils.student.annotation.StudentImplementationRequired;
+
+import static org.tudalgo.algoutils.student.Student.crash;
+
 /**
  * Main entry point in executing the program.
  */
@@ -10,6 +15,45 @@ public class Main {
      * @param args program arguments, currently ignored
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        testHeader("Multiplication Table");
+        numberExpressionMultiplicationTableTests();
+
+        testHeader("Peano Number Expressions");
+        peanoNumberExpressionTests();
+
+        testHeader("Filter, Fold, Map");
+        filterFoldMapTests();
+    }
+
+    @DoNotTouch
+    private static void testHeader(String testName) {
+        System.out.println("-----------------------------------");
+        System.out.println("Running test: " + testName);
+        System.out.println("-----------------------------------");
+    }
+
+    @DoNotTouch
+    private static void numberExpressionMultiplicationTableTests() {
+        // TODO: H2.2 - uncomment to test
+//        int lowerBound = 1;
+//        int upperBound = 15;
+//        NumberExpression[] multiplicationTable = NumberExpressionFactory.multiplicationTable(lowerBound, upperBound);
+//
+//        for (int i = lowerBound; i <= upperBound; i++) {
+//            for (int j = lowerBound; j <= upperBound; j++) {
+//                System.out.printf("| %4s ", multiplicationTable[(i - lowerBound) * (upperBound - lowerBound + 1) + (j - lowerBound)].evaluate());
+//            }
+//            System.out.println("|");
+//        }
+    }
+
+    @StudentImplementationRequired
+    private static void peanoNumberExpressionTests() {
+        crash(); // TODO: H3.3 - remove if implemented
+    }
+
+    @StudentImplementationRequired
+    private static void filterFoldMapTests() {
+        crash(); // TODO: H4.5 - remove if implemented
     }
 }
